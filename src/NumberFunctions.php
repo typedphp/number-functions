a8e8f4faa98036572e06041010ebb18f72f65fe0
Christopher Pitt <cgpitt@gmail.com>
2014-12-16 10:45:39 +0200

    Added number methods

--- src/NumberFunctions.php
+++ src/NumberFunctions.php
@@ -1,3 +1,266 @@
 <?php
 
 namespace TypedPHP\Functions\NumberFunctions;
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function absolute($number)
+{
+    return (float) \abs($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function arcCosine($number)
+{
+    return (float) \acos($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function arcSine($number)
+{
+    return (float) \asin($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function arcTangent($number)
+{
+    return (float) \atan($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function cosine($number)
+{
+    return (float) \cos($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function exponent($number)
+{
+    return (float) \exp($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function hyperbolicCosine($number)
+{
+    return (float) \cosh($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function hyperbolicSine($number)
+{
+    return (float) \sinh($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function hyperbolicTangent($number)
+{
+    return (float) \tanh($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function arcHyperbolicCosine($number)
+{
+    return (float) \acosh($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function arcHyperbolicSine($number)
+{
+    return (float) \asinh($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function arcHyperbolicTangent($number)
+{
+    return (float) \atanh($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function logarithm($number)
+{
+    return (float) \log($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function sine($number)
+{
+    return (float) \sin($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function squareRoot($number)
+{
+    return (float) \sqrt($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function tangent($number)
+{
+    return (float) \tan($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function degrees($number)
+{
+    return (float) \rad2deg($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function radians($number)
+{
+    return (float) \deg2rad($number);
+}
+
+/**
+ * @param float|int $number
+ * @param float|int $divisor
+ *
+ * @return float
+ */
+function modulus($number, $divisor)
+{
+    return (float) \fmod($number, $divisor);
+}
+
+/**
+ * @param float|int $number
+ * @param float|int $power
+ *
+ * @return float
+ */
+function power($number, $power)
+{
+    return (float) \pow($number, $power);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function round($number)
+{
+    return (float) \round($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function ceiling($number)
+{
+    return (float) \ceil($number);
+}
+
+/**
+ * @param float|int $number
+ *
+ * @return float
+ */
+function floor($number)
+{
+    return (float) \floor($number);
+}
+
+/**
+ * @param int $min
+ * @param int $max
+ *
+ * @return int
+ */
+function random($min, $max)
+{
+    return (int) \mt_rand($min, $max);
+}
+
+/**
+ * @param $number
+ * @param $min
+ * @param $max
+ *
+ * @return mixed
+ */
+function limit($number, $min, $max)
+{
+    if ($number < $min) {
+        return $min;
+    }
+
+    if ($number > $max) {
+        return $max;
+    }
+
+    return $number;
+}
--- tests/NumberFunctionTest.php
+++ tests/NumberFunctionTest.php
@@ -6,5 +6,136 @@ use TypedPHP\Functions\NumberFunctions;
 
 class NumberFunctionTest extends Test
 {
+    public function testRound()
+    {
+        $this->assertEquals(1, NumberFunctions\round(1.4));
+        $this->assertEquals(2, NumberFunctions\round(1.5));
+    }
 
+    public function testCeiling()
+    {
+        $this->assertEquals(2, NumberFunctions\ceiling(1.4));
+    }
+
+    public function testFloor()
+    {
+        $this->assertEquals(1, NumberFunctions\floor(1.5));
+    }
+
+    public function testAbsolute()
+    {
+        $this->assertEquals(0.5, NumberFunctions\absolute(-0.5));
+    }
+
+    public function testArcCosine()
+    {
+        $this->assertEquals(acos(0.5), NumberFunctions\arcCosine(0.5));
+    }
+
+    public function testArcSine()
+    {
+        $this->assertEquals(asin(0.5), NumberFunctions\arcSine(0.5));
+    }
+
+    public function testArcTangent()
+    {
+        $this->assertEquals(atan(0.5), NumberFunctions\arcTangent(0.5));
+    }
+
+    public function testHyperbolicCosine()
+    {
+        $this->assertEquals(cosh(0.5), NumberFunctions\hyperbolicCosine(0.5));
+    }
+
+    public function testHyperbolicSine()
+    {
+        $this->assertEquals(sinh(0.5), NumberFunctions\hyperbolicSine(0.5));
+    }
+
+    public function testHyperbolicTangent()
+    {
+        $this->assertEquals(tanh(0.5), NumberFunctions\hyperbolicTangent(0.5));
+    }
+
+    public function testInverseHyperbolicCosine()
+    {
+        $this->assertEquals(acosh(1.5), NumberFunctions\arcHyperbolicCosine(1.5));
+    }
+
+    public function testInverseHyperbolicSine()
+    {
+        $this->assertEquals(asinh(0.5), NumberFunctions\arcHyperbolicSine(0.5));
+    }
+
+    public function testInverseHyperbolicTangent()
+    {
+        $this->assertEquals(atanh(0.5), NumberFunctions\arcHyperbolicTangent(0.5));
+    }
+
+    public function testLogarithm()
+    {
+        $this->assertEquals(log(0.5), NumberFunctions\logarithm(0.5));
+    }
+
+    public function testModulus()
+    {
+        $this->assertEquals(fmod(1.5, 0.5), NumberFunctions\modulus(1.5, 0.5));
+    }
+
+    public function testPower()
+    {
+        $this->assertEquals(pow(1.5, 0.5), NumberFunctions\power(1.5, 0.5));
+    }
+
+    public function testSine()
+    {
+        $this->assertEquals(sin(0.5), NumberFunctions\sine(0.5));
+    }
+
+    public function testSquareRoot()
+    {
+        $this->assertEquals(sqrt(0.5), NumberFunctions\squareRoot(0.5));
+    }
+
+    public function testTangent()
+    {
+        $this->assertEquals(tan(0.5), NumberFunctions\tangent(0.5));
+    }
+
+    public function testDegrees()
+    {
+        $this->assertEquals(rad2deg(0.5), NumberFunctions\degrees(0.5));
+    }
+
+    public function testRadians()
+    {
+        $this->assertEquals(deg2rad(0.5), NumberFunctions\radians(0.5));
+    }
+
+    public function testRandom()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $random = NumberFunctions\random(5 * $i, 10 * $i);
+
+            $this->assertGreaterThanOrEqual(5 * $i, $random);
+            $this->assertLessThanOrEqual(10 * $i, $random);
+        }
+    }
+
+    public function testLimit()
+    {
+        $this->assertEquals(5, NumberFunctions\limit(3, 5, 10));
+        $this->assertEquals(10, NumberFunctions\limit(12, 5, 10));
+        $this->assertEquals(7, NumberFunctions\limit(7, 5, 10));
+    }
+
+    public function testExponent()
+    {
+        $this->assertEquals(exp(0.5), NumberFunctions\exponent(0.5));
+    }
+
+    public function testCosine()
+    {
+        $this->assertEquals(cos(0.5), NumberFunctions\cosine(0.5));
+    }
 }
